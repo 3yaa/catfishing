bf9dcc7d5c45b25b392969367787876a55eee899
3yaa <3yaa@users.noreply.github.com>
2025-11-13 22:31:43 -0800

    the code is done?

--- scripts/blackjack.gd
+++ scripts/blackjack.gd
@@ -1,11 +1,32 @@
 class_name Blackjack
 
-enum GameState { BETTING, DEALING, PLAYER_TURN, DEALER_TURN, RESOLVE }
-
-var deck: Deck = Deck.new()
-var player_hand: Array[Card] = []
-var fish_hand: Array[Card] = []
-var game_state: GameState = GameState.DEALING
+var deck: Deck
+var hand: Array[Deck.Card] = []
+var is_bust: bool = false
+var is_standing: bool = false
 
 func _init():
-		
+	hand.clear()
+	deck = Deck.new()
+	hand.append(deck.draw_card())
+	hand.append(deck.draw_card())
+
+func is_finished() -> bool:
+	return is_bust || is_standing
+# 
+func get_score() -> int:
+	var score = 0
+	for card in hand:
+		score += card.value
+	return score
+
+func hit():
+	if is_finished():
+		return
+	# 
+	hand.append(deck.draw_card())
+	if get_score() > 21:
+		is_bust = true
+
+func stand():
+	is_standing = true
---
+++ scripts/minigame_manager.gd
@@ -0,0 +1,48 @@
+class_name MinigameManager
+extends Node
+
+@export var max_game_num: int = 4
+@export var score_to_catch: int = 100
+# 
+var current_game: Blackjack
+var player_score: int = 10
+var cur_game_bet: int = 0
+var cur_game_num: int = 1
+# 
+signal caught_fish
+signal lost_fish
+signal cur_game_finished(player_score: int)
+
+func _ready():
+	new_game()
+
+func new_game():
+	# -- won
+	if player_score >= score_to_catch:
+		caught_fish.emit()
+		return
+	# -- lost
+	if cur_game_num >= max_game_num:
+		lost_fish.emit()
+		return
+	
+	# new hand
+	cur_game_num += 1
+	current_game = Blackjack.new()
+
+func finish_game():
+	var cur_game_score = 0
+	if not current_game.is_bust:
+		cur_game_score = int(current_game.get_score() * _get_payout_multiplier())
+	
+	player_score += cur_game_score
+	cur_game_finished.emit(player_score)
+
+	# await get_tree().create_timer(1.0).timeout
+	new_game()
+
+func _get_payout_multiplier() -> float:
+	if current_game.is_bust:
+		return 0.0
+	# LOGIC FOR MULTIPLIER 
+	return 1.0
