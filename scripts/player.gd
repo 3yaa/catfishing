b8e31fd071ee47c3e93c220ef4046c036f9ff7ea
loctng <lctnguyen@ucdavis.edu>
2025-11-26 14:48:23 -0800

    disable jump in ocean

--- scripts/player.gd
+++ scripts/player.gd
@@ -38,7 +38,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and is_on_floor() and not is_in_ocean:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
