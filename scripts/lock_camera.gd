104867939e2caef7ac20d4e784e75da302066661
3yaa <93301444+3yaa@users.noreply.github.com>
2025-12-07 01:42:20 -0800

    game background moved

--- scripts/lock_camera.gd
+++ scripts/lock_camera.gd
@@ -5,4 +5,4 @@ extends Camera2D
 
 func _physics_process(_delta: float) -> void:
 	if target:
-		global_position = Vector2(target.global_position.x, target.global_position.y-100)
+		global_position = Vector2(target.global_position.x, target.global_position.y - 170)
