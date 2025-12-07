57bc13a0bc73db2b238b04bf7b4541386afa5240
3yaa <93301444+3yaa@users.noreply.github.com>
2025-12-06 20:06:41 -0800

    new cards implemented

--- scripts/minigame/card_display.gd
+++ scripts/minigame/card_display.gd
@@ -1,26 +1,25 @@
 extends TextureRect
 
 func _ready():
-	custom_minimum_size = Vector2(80, 120)
-	expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL
+	custom_minimum_size = Vector2(100, 140)
+	expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 
 func set_card(suit: String, rank: String):
-	# converts score to asset
-	var rank_str = rank
-	if rank in ["2", "3", "4", "5", "6", "7", "8", "9"]:
-		rank_str = "0" + rank
+	# Capitalize first letter of suit: Hearts, Diamonds, etc.
+	var suit_capitalized = suit.capitalize()
 	
-	# convert suit name to lowercase
-	var suit_lower = suit.to_lower()
+	# Build the card path: cardHearts_A.png, cardDiamonds_5.png, etc.
+	var card_path = "res://assets/Black Jack/cards/card%s_%s.png" % [suit_capitalized, rank]
 	
-	# now build card path
-	var card_path = "res://assets/Cards (large)/card_%s_%s.png" % [suit_lower, rank_str]
-	
-	# load texture
+	# Load the texture
 	texture = load(card_path)
+	if not texture:
+		push_error("Card image not found: " + card_path)
 
 func set_card_hidden():
-	# back image of card
-	var back_path = "res://assets/Cards (large)/card_back.png"
+	# Load the card back image
+	var back_path = "res://assets/Black Jack/cards/cardBack.png"
 	texture = load(back_path)
+	if not texture:
+		push_error("Card back image not found: " + back_path)
