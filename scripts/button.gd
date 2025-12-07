5ee7cc3b5e10484710f5396944cec8bb97f122b6
3yaa <93301444+3yaa@users.noreply.github.com>
2025-12-06 18:08:40 -0800

    main menu basically finished

---
+++ scripts/button.gd
@@ -0,0 +1,14 @@
+extends Button
+
+var original_y = 0.0
+var time = 0.0
+
+func _ready():
+	original_y = position.y
+
+func _physics_process(delta):
+	time += delta
+	# Linear up and down - no slow down at peaks
+	var cycle = fmod(time * 3, PI * 2)
+	var triangle = abs((cycle / PI) - 1.0) * 2.0 - 1.0
+	position.y = original_y + triangle * 8
---
+++ scripts/main_menu.gd
@@ -0,0 +1,71 @@
+extends CanvasLayer
+@onready var day_bg = $DayBackground
+@onready var night_bg = $NightBackground
+@onready var player = $Player
+@onready var sprite = $Player/AnimatedSprite2D
+
+var screen_width = 1920
+var walk_duration = 20.0 # seconds to cross screen
+# 
+var transition_time = 2.0 # seconds for fade
+var hold_time = 10.0 # seconds to hold each image
+var timer = 0.0
+var is_day = true
+
+func _ready():
+	night_bg.modulate.a = 0.0 # start wiht day
+	# 
+	player.position.x = 0
+	player.position.y = 1000
+	sprite.play("idle_ocean")
+	start_walking()
+
+func start_walking():
+	walk_right()
+
+func _process(delta):
+	timer += delta
+	
+	# hold current image
+	if timer < hold_time:
+		return
+	
+	# transition phase
+	var transition_progress = (timer - hold_time) / transition_time
+	
+	if transition_progress >= 1.0:
+		# transition complete -> switch and reset
+		is_day = !is_day
+		timer = 0.0
+		
+		if is_day:
+			night_bg.modulate.a = 0.0
+		else:
+			night_bg.modulate.a = 1.0
+	else:
+		if is_day:
+			# fade to night
+			night_bg.modulate.a = transition_progress
+		else:
+			# fade to day
+			night_bg.modulate.a = 1.0 - transition_progress
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+# player
+func walk_right():
+	sprite.flip_h = false
+	var tween = create_tween()
+	tween.tween_property(player, "position:x", screen_width, walk_duration)
+	tween.tween_callback(walk_left)
+
+func walk_left():
+	sprite.flip_h = true
+	var tween = create_tween()
+	tween.tween_property(player, "position:x", 0, walk_duration)
+	tween.tween_callback(walk_right)
